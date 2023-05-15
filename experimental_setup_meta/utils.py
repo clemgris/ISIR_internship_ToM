ff78a43ce23e7bd607c5c02861eadebae04717d1
clemgris <clemence.grislain@eleves.enpc.fr>
2023-05-15 17:44:23 +0200

    Type the util functions

--- experimental_setup_meta/utils.py
+++ experimental_setup_meta/utils.py
@@ -3,8 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import log
+from typing import *
 
-def draw(proba_dist):
+def draw(proba_dist: np.array) -> int:
     assert(np.isclose(proba_dist.sum(), 1.))
     rand_num = np.random.uniform(0, 1)
     cum_prob = np.cumsum(proba_dist)
@@ -14,20 +15,20 @@ def draw(proba_dist):
             break
     return selected_idx
 
-def render_policy(policy):
+def render_policy(policy: np.array) -> None:
     plt.imshow(policy.reshape(-1,1).T, extent=[0, policy.shape[0], 0, 1], vmin=0, vmax=1.)
     plt.grid(True, which='both', color='w', linewidth=1)
     plt.xticks(np.arange(policy.shape[0]))
     plt.yticks([])
     plt.show()
 
-def render_trajectory(env, traj):
+def render_trajectory(env, traj: tuple) -> None:
     for a,_ in zip(traj[0], traj[1]):
         env.render()
         plt.scatter(a + 0.5, 0.5, c='b')
         plt.show()
 
-def Shannon_entropy(proba_dist,axis=None):
+def Shannon_entropy(proba_dist: np.array, axis: int=None) -> Union[float, np.array]:
     # Compute the Shannon Entropy 
     tab = proba_dist * np.log2(proba_dist)
     tab[np.isnan(tab)] = 0
