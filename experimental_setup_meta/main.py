9551e68048c27f32cf34bd777f6cd20f30d94e37
clemgris <clemence.grislain@eleves.enpc.fr>
2023-05-22 15:05:45 +0200

    Add training arguments parser

--- experimental_setup_meta/main.py
+++ experimental_setup_meta/main.py
@@ -1,52 +1,66 @@
 import numpy as np
 import os
-import torch.optim as optim
 import torch
+import argparse
+
+import torch.optim as optim
 
 from storage import Storage, load_data
 from dataset import ToMNetDataset
 from torch.utils.data import DataLoader
 from model import CharNet, MentalNet, PredNet
 
-loading_path = './data/22-05-2023'
-config = load_data(os.path.join(loading_path,'config_dataset.json'))
+def parse_args():
+    parser = argparse.ArgumentParser('Saving data')
+    parser.add_argument('--n_epochs', '-e', type=int, default=10)
+    parser.add_argument('--batch_size', '-b', type=int, default=3)
+    parser.add_argument('--learning_rate', '-lr', type=int, default=0.001)
+    args = parser.parse_args()
+    return args
 
-device = 'cpu' if torch.cuda.is_available() else 'cuda'
 
-# Dataset parameters
-n_buttons, n_music = config['n_buttons'], config['n_music']
-num_past, max_steps, min_steps = config['num_past'], config['max_steps'], config['min_steps']
-n_agent_train, n_agent_test = config['n_agent_train'], config['n_agent_test']
+if __name__ == '__main__':
+    args = parse_args()
+        
+    loading_path = './data/22-05-2023'
+    config = load_data(os.path.join(loading_path,'config_dataset.json'))
 
-# Load data
-train_data =load_data(os.path.join(loading_path, 'train_dataset.json')) 
-test_data = load_data(os.path.join(loading_path, 'test_dataset.json'))
+    device = 'cpu' if torch.cuda.is_available() else 'cuda'
 
-train_dataset = ToMNetDataset(**train_data)
-print('{} training data'.format(len(train_data['target_actions'])))
+    # Dataset parameters
+    n_buttons, n_music = config['n_buttons'], config['n_music']
+    num_past, max_steps, min_steps = config['num_past'], config['max_steps'], config['min_steps']
+    n_agent_train, n_agent_test = config['n_agent_train'], config['n_agent_test']
 
-test_dataset = ToMNetDataset(**test_data)
-print('{} test data'.format(len(test_data['target_actions'])))
+    # Load data
+    train_data =load_data(os.path.join(loading_path, 'train_dataset.json')) 
+    test_data = load_data(os.path.join(loading_path, 'test_dataset.json'))
 
-# Training parameters
-batch_size = 3
-learning_rate = 0.001
-n_epochs = 50
+    train_dataset = ToMNetDataset(**train_data)
+    print('{} training data'.format(len(train_data['target_actions'])))
 
-# Load data and model 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    test_dataset = ToMNetDataset(**test_data)
+    print('{} test data'.format(len(test_data['target_actions'])))
 
-prednet = PredNet(num_input=2, num_agent=n_agent_train, num_step=max_steps, n_buttons=n_buttons, device=device)
-optimizer = optim.Adam(prednet.parameters(), lr=learning_rate)
+    # Training parameters
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
+    n_epochs = args.n_epochs
 
-# Training loop
-for epoch in range(n_epochs):
-    train_dict = prednet.train(train_loader, optimizer)
-    train_msg ='Train| Epoch {} Loss | {:.4f} | Acc | {:.4f} |'.format(epoch, train_dict['loss'], train_dict['accuracy'])
-    print(train_msg)
+    # Load data and model 
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-test_dict = prednet.evaluate(test_loader)
+    prednet = PredNet(num_input=2, num_agent=n_agent_train, num_step=max_steps, n_buttons=n_buttons, device=device)
+    optimizer = optim.Adam(prednet.parameters(), lr=learning_rate)
 
-test_msg ='Test| Epoch {} Loss | {:.4f} | Acc | {:.4f} |'.format(epoch, test_dict['loss'], test_dict['accuracy'])
-print(test_msg)
+    # Training loop
+    for epoch in range(n_epochs):
+        train_dict = prednet.train(train_loader, optimizer)
+        train_msg ='Train| Epoch {} Loss | {:.4f} | Acc | {:.4f} |'.format(epoch, train_dict['loss'], train_dict['accuracy'])
+        print(train_msg)
+
+    test_dict = prednet.evaluate(test_loader)
+
+    test_msg ='Test| Epoch {} Loss | {:.4f} | Acc | {:.4f} |'.format(epoch, test_dict['loss'], test_dict['accuracy'])
+    print(test_msg)
