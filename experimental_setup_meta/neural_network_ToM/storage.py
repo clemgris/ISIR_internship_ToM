dc345dd946a3f1a8a08c927af53f5b5df9588585
clemgris <clemence.grislain@eleves.enpc.fr>
2023-05-23 14:20:12 +0200

    Move saving/loading function into utils.py

--- experimental_setup_meta/neural_network_ToM/storage.py
+++ experimental_setup_meta/neural_network_ToM/storage.py
@@ -1,7 +1,3 @@
-import json
-import os
-
-from datetime import datetime
 import numpy as np
 
 from learner import Learner
@@ -87,38 +83,3 @@ class Storage:
         self.current_traj = np.zeros([self.length, self.max_steps, self.n_buttons, 2])
         self.demo = np.zeros([self.num_agents * self.num_types, self.max_steps, self.n_buttons, 2])
         self.target_actions = np.zeros([self.num_agents * self.num_types, self.n_buttons])
-
-
-def save_data(dict, mode):
-
-    data_dict = dict.copy()
-
-    date = datetime.now().strftime('%d-%m-%Y')
-    dir = f'./data/{date}'
-    make_dirs(dir)
-    path = dir + '/' + f'{mode}_dataset.json'
-
-    # Convert np.array into list
-    for key in data_dict.keys():
-        if isinstance(data_dict[key], np.ndarray):
-            data_dict[key] = data_dict[key].tolist()
-
-    with open(path, "w") as f:
-        json.dump(data_dict, f)
-
-def load_data(path):
-    with open(path, "r") as f:
-        data_dict = json.load(f)
-    
-    # Convert list into np.array
-    for key in data_dict.keys():
-        if isinstance(data_dict[key], list):
-            data_dict[key] = np.array(data_dict[key])
-    return data_dict
-
-def make_dirs(path):
-    try:
-        os.makedirs(path)
-    except OSError:
-        if not os.path.isdir(path):
-            raise
