ed0db1c41f44a1a6c6eb0e50a4a903946c538e0a
clemgris <clemence.grislain@eleves.enpc.fr>
2023-05-23 18:01:41 +0200

    Remove recycled trajectories

--- experimental_setup_meta/neural_network_ToM/storage.py
+++ experimental_setup_meta/neural_network_ToM/storage.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+import sys
+sys.path.append('/home/chetouani/Documents/STAGE_Clemence/ISIR_internship_ToM/experimental_setup_meta')
+
 from learner import Learner
 from environment import ButtonsToy
 
@@ -31,45 +34,41 @@ class Storage:
         for type in range(self.num_types):
             agent = Learner(type=type)
             for n_agent in range(self.num_agents):
-                # Ravel
-                idx = type * self.num_agents * self.num_demo_types + n_agent * self.num_demo_types
-                
-                # Store past trajectories
-                for nn in range(self.num_past):
-                    past_env = ButtonsToy(self.n_buttons, self.n_music)
-                    agent.init_env(past_env)
-                    actions, rewards = agent.act(size=self.max_steps)
-                    # actions (one-hot encoding)
-                    self.past_traj[idx, nn, np.arange(self.max_steps), actions, 0] = 1                          
-                    # rewards
-                    self.past_traj[idx, nn, np.arange(self.max_steps), :, 1] = np.tile(rewards, (self.n_buttons, 1)).T
-                
-                # Store current trajectory
-                current_env = ButtonsToy(self.n_buttons, self.n_music)
-                agent.init_env(current_env)
-                num_steps = np.random.randint(self.min_steps, self.max_steps)
-                actions, rewards = agent.act(size=num_steps)
-                self.current_traj[idx, np.arange(num_steps), actions, 0] = 1
-                self.current_traj[idx, np.arange(num_steps), :, 1] = np.tile(rewards, (self.n_buttons, 1)).T
-                
-                idx_music = np.where(np.isclose(current_env.R, 1.))[0]
                 for demo_type in range(self.num_demo_types):
+                    # Ravel
+                    idx = type * self.num_agents * self.num_demo_types + n_agent * self.num_demo_types + demo_type
+                    # Store past trajectories
+                    for nn in range(self.num_past):
+                        past_env = ButtonsToy(self.n_buttons, self.n_music)
+                        agent.init_env(past_env)
+                        actions, rewards = agent.act(size=self.max_steps)
+                        # actions (one-hot encoding)
+                        self.past_traj[idx, nn, np.arange(self.max_steps), actions, 0] = 1                          
+                        # rewards
+                        self.past_traj[idx, nn, np.arange(self.max_steps), :, 1] = np.tile(rewards, (self.n_buttons, 1)).T
+                
+                    # Store current trajectory
+                    current_env = ButtonsToy(self.n_buttons, self.n_music)
+                    agent.init_env(current_env)
+                    num_steps = np.random.randint(self.min_steps, self.max_steps)
+                    actions, rewards = agent.act(size=num_steps)
+                    self.current_traj[idx, np.arange(num_steps), actions, 0] = 1
+                    self.current_traj[idx, np.arange(num_steps), :, 1] = np.tile(rewards, (self.n_buttons, 1)).T
+
                     # Reset agent believes
                     agent.init_env(current_env)
-                    # Copy past and current trajectories
-                    self.past_traj[idx + demo_type] = self.past_traj[idx, nn].copy()                    
-                    self.current_traj[idx + demo_type] = self.current_traj[idx].copy()
                     # Create demo
+                    idx_music = np.where(np.isclose(current_env.R, 1.))[0]
                     if demo_type == 0:
                         actions, rewards = (np.arange(current_env.n_buttons), current_env.R)
                     else:
                         actions, rewards = (np.random.choice(idx_music, size = demo_type, replace=False), [1.] * demo_type)
-                    self.demonstrations[idx + demo_type, np.arange(actions.shape[0]), actions, 0] = 1
-                    self.demonstrations[idx + demo_type, np.arange(actions.shape[0]), :, 1] = np.tile(rewards, (self.n_buttons, 1)).T
+                    self.demonstrations[idx, np.arange(actions.shape[0]), actions, 0] = 1
+                    self.demonstrations[idx, np.arange(actions.shape[0]), :, 1] = np.tile(rewards, (self.n_buttons, 1)).T
                     # Target action
                     agent.observe((actions, rewards))
                     action, _ = agent.act(size=1)
-                    self.target_actions[idx + demo_type] = action[0]
+                    self.target_actions[idx] = action[0]
         
         return dict(
             past_traj = self.past_traj,
