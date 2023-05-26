0bc250aff0a3ee6700791753ece26de2e559596f
clemgris <clemence.grislain@eleves.enpc.fr>
2023-05-26 17:55:49 +0200

    Clean

--- experimental_setup_meta/neural_network_ToM/nn_utils.py
+++ experimental_setup_meta/neural_network_ToM/nn_utils.py
@@ -13,41 +13,30 @@ def make_dirs(path):
 
 def save_config(dico: dict, mode: str, saving_name: str) -> None:
     data_dict = dico.copy()
-
     if saving_name is None:
         saving_name = datetime.now().strftime('%d-%m-%Y')
-
     dir = f'./data/{saving_name}'
     make_dirs(dir)
     path = dir + '/' + f'{mode}_dataset.json'
-
     with open(path, 'w') as f:
         json.dump(data_dict, f)
 
 def load_config(path: str) -> dict:
-    
     with open(path, 'rb') as f:
         data_dict = json.load(f)
-
     return data_dict
 
 def save_data(dico: dict, mode: str, saving_name: str) -> None:
-
     data_dict = dico.copy()
-
     if saving_name is None:
         saving_name = datetime.now().strftime('%d-%m-%Y')
-
     dir = f'./data/{saving_name}'
     make_dirs(dir)
     path = dir + '/' + f'{mode}_dataset.pickle'
-
     with open(path, 'wb') as handle:
         pickle.dump(data_dict, handle)
 
 def load_data(path: str) -> dict:
-
     with open(path, 'rb') as handle:
         data_dict = pickle.load(handle)
-
     return data_dict
