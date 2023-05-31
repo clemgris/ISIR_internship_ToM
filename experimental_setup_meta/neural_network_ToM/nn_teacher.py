e6f2b4c610c5594a1e3c68a16cda1ee77c2d601d
clemgris <clemence.grislain@eleves.enpc.fr>
2023-05-31 17:39:08 +0200

    Implement ToM teacher based on ToMNet trained model

--- experimental_setup_meta/neural_network_ToM/nn_teacher.py
+++ experimental_setup_meta/neural_network_ToM/nn_teacher.py
@@ -1,10 +1,85 @@
 
 import numpy as np
+import torch
+import os
+import sys
 
-from learner import compute_policy, bayesian_update, projection
 from environment import ButtonsToy
 from teacher import Teacher, cost
 
+from neural_network_ToM.model import PredNet
+from neural_network_ToM.nn_utils import load_config
+
 # ToM teacher: NN model of the learner
-def ToMNetTeacher(Teacher):
-    pass
+class ToMNetTeacher(Teacher):
+    
+    def __init__(self, env: ButtonsToy, num_types: int, max_steps: int, loading_path: str, criterion: str='loss', device: str=None) -> None:
+        super().__init__(env, num_types)
+
+        model_loading_path = os.path.join(loading_path, f'prednet_model_best_{criterion}.pt')
+        if device == None:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        model_config = load_config(os.path.join(loading_path, f'config_best_{criterion}.json'))
+
+        data_loading_path = os.path.join('neural_network_ToM', model_config['data_path'])
+        config = load_config(os.path.join(data_loading_path, 'config_dataset.json'))
+
+        assert(config['n_buttons'] == env.n_buttons)
+        self.max_steps = config['max_steps']
+
+        self.model = PredNet(num_input=2,
+                        num_step=max_steps, 
+                        n_buttons=env.n_buttons,
+                        num_output_char=model_config['e_char_dim'],
+                        basic_layer= model_config['basic_layer'],
+                        device=device)
+
+        self.model.load_state_dict(torch.load(model_loading_path))
+        self.init_env(env)
+
+    def init_env(self, env: ButtonsToy) -> None:
+        super().init_env(env)
+
+    def observe(self, traj: tuple) -> None:
+        assert(len(traj[0]) <= self.max_steps)
+
+        one_hot_traj = np.zeros([self.max_steps, self.env.n_buttons, 2])
+        for ii,pair in enumerate(zip(traj[0], traj[1])):
+            u, r = pair
+            one_hot_traj[ii, u, 0] = 1
+            one_hot_traj[ii, :, 1] = np.ones(self.env.n_buttons) * r
+
+        self.observed_traj = one_hot_traj[None,...]
+        self.current_traj = np.zeros([self.max_steps, self.env.n_buttons, 2])
+
+    def predict_learner_type(self) -> int:
+        pass # Could leverage e_char embedding (clustering)
+
+    def predict_reward(self, demonstration: tuple) -> float:
+        one_hot_demo = np.zeros([self.env.n_buttons, self.env.n_buttons, 2])
+        for ii,pair in enumerate(zip(demonstration[0], demonstration[1])):
+            a, r = pair
+            one_hot_demo[ii, a, 0] = 1
+            one_hot_demo[ii, :, 1] = np.ones(self.env.n_buttons) * r
+        
+        # Convert into tensors
+        observed_traj_tensor = torch.from_numpy(self.observed_traj[None,...])
+        current_traj_tensor = torch.from_numpy(self.current_traj[None,...])
+        demo_tensor = torch.from_numpy(one_hot_demo[None,...])
+
+        # Model inference
+        pred_action, _, _, _ = self.model(observed_traj_tensor, current_traj_tensor, demo_tensor)
+
+        predicted_reward = np.sum(pred_action.detach().numpy() * self.env.R)
+        return predicted_reward
+
+    def demonstrate(self, method: str=None, alpha: float=0, true_learner_type: int=None) -> tuple:
+        # Compute utilities of each demonstration
+        utilities = np.zeros(self.num_demo_type)
+        for ii,demo in enumerate(self.demonstrations):
+            utilities[ii] = self.predict_reward(demo) - cost(demo, alpha=alpha)
+        
+        argmax_set = np.where(np.isclose(utilities, np.max(utilities)))[0]
+        selected_idx = np.random.choice(argmax_set)
+        return self.demonstrations[selected_idx]
